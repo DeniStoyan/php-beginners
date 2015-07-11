e5d4b3266a86db89928009e7b25a421ab4c3ead0
slavi <slavijekov@gmail.com>
2015-07-11 08:53:14 +0300

    edno

--- lesson1/index.php
+++ lesson1/index.php
@@ -1,6 +1,6 @@
 <html>
 <?php
-    $title = ""; // Site title
+    $title = ""; // Site title11111
     $pageTitle = ""; // This page title
     $aboutPath = ""; // Relative path to our about page
     $today = ""; // What day is today
