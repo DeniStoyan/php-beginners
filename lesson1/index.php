08b9d48dc3af7c67dd69691e8e8356a4047dd4a3
slavijekov <slavijekov@gmail.com>
2015-07-11 08:34:32 +0300

    Update index.php

--- lesson1/index.php
+++ lesson1/index.php
@@ -1,6 +1,6 @@
 <html>
 <?php
-    $title = ""; // Site title23
+    $title = ""; // Site title
     $pageTitle = ""; // This page title
     $aboutPath = ""; // Relative path to our about page
     $today = ""; // What day is today
